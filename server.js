7b2847c1574b8ef2f18167da12cab54553c55316
Ajil Abdalqader <ajilqado@gmail.com>
2021-05-15 23:32:44 +0200

    passing.text-from-server-to-htmlAsVariable-with-ejs

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 app.set('view engine', 'ejs')
 app.use('/articles', articleRouter)
 app.get('/', (req, res) => {
-  res.render("index")
+  res.render("index", {text:"Hello"})
 })
 
 app.listen(5000)
