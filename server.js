d0d59051dd1fef059393d3b9b7d8112d64f16822
Ajil Abdalqader <ajilqado@gmail.com>
2021-05-16 01:47:29 +0200

    creating-routes-for-New-Articles

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
         description:"test description"
     }]
     
-  res.render("index", {articles: articles})
+  res.render("articles/index", {articles: articles})
 })
 
 app.listen(5000)
