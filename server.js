ed7ebb493dd074ad286f0bf7032ee47f7d7c0bda
Ajil Abdalqader <ajilqado@gmail.com>
2021-05-15 23:42:59 +0200

    adding-bootstrap

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const app = express()
 app.set('view engine', 'ejs')
 app.use('/articles', articleRouter)
 app.get('/', (req, res) => {
-  res.render("index", {text:"Hello"})
+    const articles=[{
+        title:"test articles",
+        createdAt:Date.now(),
+        description:"test description"
+    }]
+  res.render("index", {articles: articles})
 })
 
 app.listen(5000)
