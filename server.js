e58f403480336700936b0b145887ae5009caeb18
Ajil Abdalqader <ajilqado@gmail.com>
2021-05-16 04:15:54 +0200

    connecting-to-dataBase-with-schema

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const articleRouter = require('./routes/articles')
 const app = express()
+
+
+
+mongoose.connect('mongodb://localhost/blog1',{
+  useNewUrlParser: true, useUnifiedTopology: true, 
+  useCreateIndex: true
+})
 
 app.set('view engine', 'ejs')
 app.use('/articles', articleRouter)
